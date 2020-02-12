b19c4ae52d26872f33f53a7305d005adf6381481
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-12 17:30:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
   new_phrase = []
-  sentence.each {|word| new_phrase << word.reverse} 
+  sentence.collect {|word| new_phrase << word.reverse} 
   
-  "#{new_phrase}"
 end 
